0ab313fe976a85d7c5a6a02df4e6088ffbee5ade
SergeyBalakirev <balakirev.sergey87@gmail.com>
2020-08-06 21:29:26 +0300

    added about section

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,16 @@
 $(function () {
 
+  $('.menu__burger').on('click', function () {
+    $('.menu__burger').toggleClass('menu__burger-active');
 
+    $('.menu__list').slideToggle();
+  });
+
+  $('.menu__link').on('click', function () {
+    $('.menu__list').slideToggle();
+
+    $('.menu__burger').toggleClass('menu__burger-active');
+  });
 
 
 });
